ba13222e5073d65b3603d01f8561ad54f75a96e0
kimpenguin <kpeng@nycopportunity.nyc.gov>
2020-12-04 16:44:39 -0500

    adding starter executable

---
+++ bin/starter.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+/**
+ * Installs the patterns and calls the scaffold command
+ */
+
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+/**
+ * Execute the passed command 
+ */
+async function run(command){
+  try {
+    await exec(command);
+  } catch {
+    (err) => {
+      console.error(err);
+    };
+  }
+}
+
+/**
+ * Installs pttrn and runs scaffold
+ */
+async function main() {
+  try {
+    console.log(`Setting up you new Patterns Project! Please wait...`);
+
+    await run(`npm i @nycopportunity/pttrn`);
+
+    await run(`npx pttrn scaffold`);
+
+    console.log(`You're all set!`);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
